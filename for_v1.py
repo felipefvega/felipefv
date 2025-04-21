e8a7b5c32c1ef75c2c8f460cf2254be0e427b9fc
felipefvega <datfiliv@gmail.com>
2025-04-21 09:40:59 -0400

    commit 21-04

---
+++ for_v1.py
@@ -0,0 +1,6 @@
+#TODOS LOS INDICES COMIEZAN EN 0
+print("Indique la cantidad de repeticiones a realizar: ")
+num = input()
+for i in range(int(num)):
+    
+ print("Repeticion ", i+1)
---
+++ tabla.py
@@ -0,0 +1,6 @@
+#TABLA DE MULTIPLICAR
+
+print("Indique un número: ")
+num = input()
+for i in range(1,11):
+    print("5 x ",i, " = ", 5*i)
